aecb1e27b3a98a58add3ab5af1462145b2ddcead
King <theo.king@sogeti.com>
2016-03-24 17:20:02 +0000

    allowed duplicate letters to now be revealed

--- HangmanGame/HangmanGame/HangmanGame.py
+++ HangmanGame/HangmanGame/HangmanGame.py
@@ -1,35 +1,36 @@
 print("Welcome to Hangman by Theo King")
 
 #variables for correct word
-answer = "Testing"
+answer = "Jujimufu"
 correct_letters = []
 hidden_letters = []
 
-
 for letter in answer:
-    correct_letters.append(letter)#.lower())
+    correct_letters.append(letter.lower())
     hidden_letters.append("_")
 
 print(" ".join(hidden_letters))
-
-
 attempts = 5
+
 while attempts > 0:
     print("Guesses left: " + str(attempts))
-
+    count = -1 
     player_guess = input("Enter a letter: \n")
-
     if player_guess in hidden_letters:
-        print("Letter already submitted")
+        print("Letter already submitted")  
     elif player_guess in correct_letters:
         print("That's correct!")
-        hidden_letters[correct_letters.index(player_guess)] = player_guess
+        for letter in correct_letters:
+            count += 1
+            if player_guess == letter:
+                hidden_letters[correct_letters.index(player_guess,count)] = player_guess
         print(" ".join(hidden_letters))
+    elif correct_letters == hidden_letters or player_guess.lower() == answer.lower():
+            print("Congratulations! You win!")
+            break
     else:
         print("Sorry, try again :(")
         attempts -= 1
-    if correct_letters == hidden_letters:
-        print("Congratulations! You win!")
-        break
+
 else:
     print("you lose")
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 #Python Hangman :snake:
 
-This is a simple hangman game written in Python. I have only just recently learned Python (started around 2 weeks ago) so this is my very first published application. I hope to find a way to integrate online dictionary to get more random words.
+This is a simple hangman game written in Python. I have only just recently learned Python so this is my very first published application. I hope to find a way to integrate an online dictionary to get more random words.
 
 > Programmed in Python 3.5.1
+> IDE used was Python Tools for Visual Studio
